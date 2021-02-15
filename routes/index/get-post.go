bd2c4a29246f9ddfc054003aa5e1061daf9b51da
DustiasTheGuy <isakwebdev@gmail.com>
2021-02-15 12:00:07 +0100

    ojqwt

--- routes/index/get-post.go
+++ routes/index/get-post.go
@@ -1,18 +1,66 @@
 package index
 
 import (
+	"isak-tech/config"
 	"isak-tech/routes"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func GetPostController(c *fiber.Ctx) error {
+	var post Post
+	var images []Image
+	db := config.DefaultConfig().ConnectMySQL()
+	row := db.QueryRow("SELECT * FROM posts WHERE id=?", c.Params("id"))
 
+	if err := row.Scan(
+		&post.ID,
+		&post.Post,
+		&post.Title,
+		&post.Category,
+		&post.Date,
+		&post.UserID,
+		&post.Archived,
+		&post.ImageURL,
+		&post.Images); err != nil {
+		log.Fatal(err)
+	}
 	// query relevant images
+
+	images = append(images, Image{
+		ID:        0,
+		URL:       post.ImageURL,
+		Date:      post.Date,
+		PostID:    post.ID,
+		Thumbnail: 1,
+	})
+
+	rows, err := db.Query("SELECT * FROM images WHERE postid=?", post.ID)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var image Image
+		if err := rows.Scan(
+			&image.ID,
+			&image.URL,
+			&image.Date,
+			&image.PostID,
+			&image.Thumbnail); err != nil {
+			log.Fatal(err)
+		}
+
+		images = append(images, image)
+	}
+
+	post.Images = images
 
 	return c.JSON(routes.HTTPResponse{
 		Message: "/get-post/:id",
 		Success: true,
-		Data:    nil,
+		Data:    post,
 	})
 }
--- routes/index/index.go
+++ routes/index/index.go
@@ -8,15 +8,23 @@ import (
 )
 
 type Post struct {
-	ID       uint      `json:"_id"`
-	Post     string    `json:"post"`
-	Title    string    `json:"title"`
-	Category string    `json:"category"`
-	Date     time.Time `json:"date"`
-	UserID   uint      `json:"userid"`
-	Archived int8      `json:"archived"`
-	ImageURL string    `json:"imageurl"`
-	Images   uint      `json:"images"`
+	ID       uint        `json:"id"`
+	Post     string      `json:"post"`
+	Title    string      `json:"title"`
+	Category string      `json:"category"`
+	Date     time.Time   `json:"date"`
+	UserID   uint        `json:"userid"`
+	Archived int8        `json:"archived"`
+	ImageURL string      `json:"-"`
+	Images   interface{} `json:"images"`
+}
+
+type Image struct {
+	ID        uint      `json:"id"`
+	URL       string    `json:"url"`
+	Date      time.Time `json:"date"`
+	PostID    uint      `json:"postid"`
+	Thumbnail int8      `json:"thumbnail"`
 }
 
 func RootController(c *fiber.Ctx) error {
